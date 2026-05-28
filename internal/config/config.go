bf4fcbaf2ecd7d95a825ebff59affa50320e9cb1
Johan Wänglöv <jwanglof@gmail.com>
2026-05-28 20:14:35 +0200

    validate exclude

--- internal/config/config.go
+++ internal/config/config.go
@@ -110,11 +110,14 @@ func Parse(data []byte) (Config, error) {
 }
 
 func validateColumn(table, col string, cc ColumnConfig) error {
-	if cc.Exclude {
-		return nil
-	}
 	hasGen := cc.Generator != ""
 	hasVal := cc.Value != nil
+	if cc.Exclude {
+		if hasGen || hasVal {
+			return fmt.Errorf("seeder.yaml: tables.%s.columns.%s: cannot set 'generator' or 'value' when 'exclude' is true", table, col)
+		}
+		return nil
+	}
 	switch {
 	case hasGen && hasVal:
 		return fmt.Errorf("seeder.yaml: tables.%s.columns.%s: cannot set both `generator` and `value`", table, col)
